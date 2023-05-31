d08cb92297ecc3bb1062a77bc46fd4e742cb614b
Đoàn Quang Huy <quanghuybest@gmail.com>
2023-05-31 18:41:57 +0700

    biểu thức chính quy

---
+++ RegEx/README.md
@@ -0,0 +1,5 @@
+# Regular Expression PHP
+## Website test nhanh
+[Link] (https://regex101.com/)
+## Link chính gốc
+[Link](https://www.php.net/manual/en/function.preg-match.php)
---
+++ RegEx/preg_match.php
@@ -0,0 +1,14 @@
+<?php
+// https://www.php.net/manual/en/function.preg-match.php
+$phone = "0859476122"; // chuỗi gần so khớp
+// ố đầu 0, các số tiếp theo từ 0-9, độ dài 9
+$pattern = "/^0[0-9]{9}$/"; // khuôn mẫu
+
+$check = preg_match($pattern, $phone);
+//var_dump($check); // 1 true, 0 false
+if (!empty($check)) {
+    echo 'Số điện thoại ' . $phone . ' hợp lệ';
+} else {
+    echo "Số điện thoại không hợp lệ";
+}
+// /0([0-9]{9})/g
