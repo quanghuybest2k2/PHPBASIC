47d23d18714580948f999f46fb896474edd8eeb9
Đoàn Quang Huy <quanghuybest@gmail.com>
2023-07-13 17:21:22 +0700

    làm được rồi

--- generator-phonetic/index.php
+++ generator-phonetic/index.php
@@ -4,9 +4,16 @@ use Linguistics\Phonetics;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$word = "Hello";
-$soundexCode = soundex($word);
-echo nl2br("$soundexCode\n");
-// ------------------------
+$word = "Hello my name is a";
 
-Phonetics::symbols($word);
+$phonetics = new Phonetics();
+$phoneticSymbols = $phonetics->symbols($word, 'array');
+
+foreach ($phoneticSymbols as $word => $symbols) {
+    echo "Từ: $word" . "<br/>";
+    foreach ($symbols as $symbol) {
+        echo nl2br("Phiên âm: $symbol\n");
+    }
+    echo "<br/>";
+}
+
