84d3f37074eaace13c571abc58a9b6a3218fc316
Doan Quang Huy <quanghuybest@gmail.com>
2024-04-10 21:38:22 +0700

    feat: function get env variable

---
+++ Env/README.md
@@ -0,0 +1,11 @@
+## Installation
+
+```shell
+cp .env.example .env
+```
+
+```shell
+php -S localhost:9999
+```
+
+## go to: http://localhost:9999
--- Env/index.php
+++ Env/index.php
@@ -1,9 +1,25 @@
 <?php
-$filename = '.env';
-if (file_exists($filename)) {
-    $env = parse_ini_file('.env');
-    $name = $env["NAME"];
-    echo $name;
-} else {
-    echo "Không tồn tại tệp $filename";
+
+function getEnvVar($name)
+{
+    $filename = ".env";
+
+    if (!file_exists($filename)) {
+        return "Không tồn tại tệp $filename";
+    }
+    $env = parse_ini_file($filename);
+    if (!isset($env[$name])) {
+        return "Không có giá trị '$name' trong tệp $filename<br/>";
+    }
+    return $env[$name];
 }
+
+echo "host name: " . getEnvVar("HOST");
+echo "<br/>";
+echo "username: " . getEnvVar("USERNAME");
+echo "<br/>";
+echo "password: " . getEnvVar("PASSWORD") . "no password";
+echo "<br/>";
+echo "database name: " . getEnvVar("DB_NAME");
+echo "<br/>";
+echo getEnvVar("ABC");
